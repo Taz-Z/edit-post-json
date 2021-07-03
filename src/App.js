af8ef9ab7128e338f87deb2c35c6e48089f062e0
Taz-Z <tanzim_zaman@hotmail.com>
2021-07-03 17:15:52 -0400

    Created a mini webpage to allow json edit and post

---
+++ src/App.js
@@ -0,0 +1,141 @@
+import { useEffect, useState } from "react";
+import ReactJson from "react-json-view";
+import { Container, Navbar, Modal } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  const [jsonVal, setJsonVal] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(window.location.search);
+    const data = queryParams.get("data");
+    if (!data) {
+      setHasError(true);
+      return;
+    }
+    const jsonString = Buffer.from(data, "base64");
+    try {
+      setJsonVal(JSON.parse(jsonString));
+    } catch (e) {
+      alert(
+        "Failed to set get JSON info, please try reclicking the discord link. If this problem persists, please contact @Developers for support"
+      );
+    }
+  }, []);
+
+  const sendMessage = () => {
+    const request = new XMLHttpRequest();
+    request.open(
+      "POST",
+      process.env.WEBHOOK_URL
+    );
+    request.setRequestHeader("Content-type", "application/json");
+
+    const queryParams = new URLSearchParams(window.location.search);
+    const f = queryParams.get("f");
+    const i = queryParams.get("i");
+    const params = {
+      content: `${f} && ${i} && ${JSON.stringify(jsonVal)}`,
+    };
+    request.send(JSON.stringify(params));
+    setIsLoading(true);
+  };
+
+  const onChange = ({
+    updated_src,
+    name,
+    namespace,
+    new_value,
+    existing_value,
+  }) => {
+    setJsonVal(updated_src);
+  };
+
+  if (hasError) {
+    return (
+      <Modal
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        show
+        onHide={() => {}}
+      >
+        <Modal.Header>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Please click the link from the embed
+          </Modal.Title>
+        </Modal.Header>
+      </Modal>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <Modal
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        show
+        onHide={() => {}}
+      >
+        <Modal.Header>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Sent your file to be uploaded, you may close your browser :)
+          </Modal.Title>
+        </Modal.Header>
+      </Modal>
+    );
+  }
+
+  return (
+    <>
+      <Navbar bg="dark" variant="dark" fixed="top">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="https://cdn.discordapp.com/icons/733117510420791358/a_2a2e9349029a56a1da08e999e0ac31e1.webp"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            GCG Price Sheet Modifiers
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+        crossOrigin="anonymous"
+      />
+      <span style={{ paddingTop: "56px", paddingBottom: "56px" }}>
+        {jsonVal && (
+          <ReactJson
+            src={jsonVal}
+            onEdit={onChange}
+            onAdd={onChange}
+            onDelete={onChange}
+            name={false}
+            validationMessage="Error doing that, do it again but better"
+            defaultValue="string"
+            quotesOnKeys={false}
+            theme="railscasts"
+            indentWidth={10}
+          />
+        )}
+      </span>
+      <Navbar bg="dark" variant="dark" fixed="bottom">
+        <Container>
+          <Navbar.Brand href="#home" onClick={sendMessage}>
+            Click here to automatically upload this file
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default App;
